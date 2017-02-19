2534f792d76d4775b5b01e62bc79bcc45ef59c5a
Edvinas <test@test.com>
2017-02-19 19:08:21 +0200

    Fix added list ID's

---
+++ app/src/main/java/com/example/android/project2/Player.java
@@ -0,0 +1,15 @@
+package com.example.android.project2;
+
+/**
+ * Created by Edvinas on 19/02/2017.
+ */
+
+public class Player {
+    private int number, score;
+
+    public Player(){}
+
+    public void setNR(int nr) {this.number = nr;}
+    public void setScore(int score) {this.score = score;}
+    public int getScore() {return score;}
+}
