26ea21b3a23fec90943ffaf07779aa60271e48e3
Edvinas <test@test.com>
2017-02-20 17:57:07 +0200

    I'm lost at this point Add player and add player points works, but can't calculate team scores

--- app/src/main/java/com/example/android/project2/MainActivity.java
+++ app/src/main/java/com/example/android/project2/MainActivity.java
@@ -5,10 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     ListView listViewA;
     PlayerAdapter playerAdapterA;
 
@@ -21,9 +21,6 @@ public class MainActivity extends AppCompatActivity {
     EditText editTeamA;
     EditText editTeamB;
 
-    TextView teamScoreA;
-    TextView teamScoreB;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +32,6 @@ public class MainActivity extends AppCompatActivity {
         editTeamA = (EditText) findViewById(R.id.teamA);
         editTeamB = (EditText) findViewById(R.id.teamB);
 
-        teamScoreA = (TextView) findViewById(R.id.scoreA);
-        teamScoreB = (TextView) findViewById(R.id.scoreB);
-
         listViewA = (ListView) findViewById(R.id.teamAList);
         playerAdapterA = new PlayerAdapter(this, R.layout.row_player);
         listViewA.setAdapter(playerAdapterA);
@@ -46,26 +40,13 @@ public class MainActivity extends AppCompatActivity {
         playerAdapterB = new PlayerAdapter(this, R.layout.row_player);
         listViewB.setAdapter(playerAdapterB);
 
-        displayScores();
     }
 
-    public void displayScores() {
-
-        for (int i = 0; i < playerAdapterB.getCount(); i++) {
-            Player player = (Player) playerAdapterB.getItem(i);
-            teamScoreB.setText(Integer.toString(player.getScore()));
-        }
-        for (int i = 0; i < playerAdapterA.getCount(); i++) {
-            Player player = (Player) playerAdapterA.getItem(i);
-            teamScoreA.setText(Integer.toString(player.getScore()));
-        }
-    }
 
     public void addPlayerA(View view) {
         if (editNumberA.getText().length() != 0 && Integer.parseInt(editNumberA.getText().toString()) > 0 && Integer.parseInt(editNumberA.getText().toString()) < 100 && editTeamA.getText().length() != 0) {
             Player player = new Player(Integer.parseInt(editNumberA.getText().toString()), editTeamA.getText().toString());
             playerAdapterA.add(player);
-            displayScores();
         } else
             Toast.makeText(MainActivity.this, "Please input player number 0-99 and a team name", Toast.LENGTH_SHORT).show();
     }
@@ -74,8 +55,6 @@ public class MainActivity extends AppCompatActivity {
         if (editNumberB.getText().length() != 0 && Integer.parseInt(editNumberB.getText().toString()) > 0 && Integer.parseInt(editNumberB.getText().toString()) < 100 && editTeamB.getText().length() != 0) {
             Player player = new Player(Integer.parseInt(editNumberB.getText().toString()), editTeamB.getText().toString());
             playerAdapterB.add(player);
-            displayScores();
-
         } else
             Toast.makeText(MainActivity.this, "Please input player number 0-99 and a team name", Toast.LENGTH_SHORT).show();
     }
--- app/src/main/java/com/example/android/project2/PlayerAdapter.java
+++ app/src/main/java/com/example/android/project2/PlayerAdapter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,62 +18,42 @@ import static com.example.android.project2.R.id.playerPTS;
  */
 
 public class PlayerAdapter extends ArrayAdapter {
-    List list = new ArrayList();
-    int pts;
-    TextView teamScoreB;
-    TextView teamScoreA;
-    EditText editTeamA;
-    EditText editTeamB;
-
-    MainActivity mainActivity = new MainActivity();
+    private List list = new ArrayList();
+    private int pts;
     public PlayerAdapter(Context context, int resource) {super(context,resource);}
 
     public void add(Player object){
         super.add(object);
         list.add(object);
     }
-
     @Override
     public int getCount() {return list.size();}
-
     @Override
     public Object getItem(int position) {return list.get(position);}
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         View row;
-        View row2;
         row = convertView;
-        row2 = convertView;
         final PlayerHolder playerHolder;
         if (row == null){
             LayoutInflater layoutInflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             row = layoutInflater.inflate(R.layout.row_player, parent, false);
-            LayoutInflater layoutInflater2 = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row2 = layoutInflater2.inflate(R.layout.activity_main,parent,false);
             playerHolder = new PlayerHolder();
-
-            teamScoreB = (TextView) row2.findViewById(R.id.scoreB);
-            teamScoreA = (TextView) row2.findViewById(R.id.scoreA);
-
-            editTeamA = (EditText) row2.findViewById(R.id.teamA);
-            editTeamB = (EditText) row2.findViewById(R.id.teamB);
 
             playerHolder.playerNumber = (TextView) row.findViewById(R.id.playerNumber);
             playerHolder.playerPTS = (TextView) row.findViewById(playerPTS);
+
             playerHolder.plusOne = (Button) row.findViewById(R.id.plusOne);
             playerHolder.plusTwo = (Button) row.findViewById(R.id.plusTwo);
             playerHolder.plusThree = (Button) row.findViewById(R.id.plusThree);
+
             playerHolder.plusOne.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     pts = Integer.parseInt(playerHolder.playerPTS.getText().toString());
                     pts++;
-                    teamScoreB.setText(Integer.toString(pts));
                     playerHolder.playerPTS.setText(Integer.toString(pts));
-
                 }
-
             });
             playerHolder.plusTwo.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -82,9 +61,7 @@ public class PlayerAdapter extends ArrayAdapter {
                     pts = Integer.parseInt(playerHolder.playerPTS.getText().toString());
                     pts += 2;
                     playerHolder.playerPTS.setText(Integer.toString(pts));
-
                 }
-
             });
             playerHolder.plusThree.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -92,30 +69,23 @@ public class PlayerAdapter extends ArrayAdapter {
                     pts = Integer.parseInt(playerHolder.playerPTS.getText().toString());
                     pts += 3;
                     playerHolder.playerPTS.setText(Integer.toString(pts));
-
                 }
-
             });
-
             row.setTag(playerHolder);
-            Player player = (Player) this.getItem(position);
-            player.setScore(pts);
         }
         else {
             playerHolder = (PlayerHolder) row.getTag();
         }
         Player player = (Player) this.getItem(position);
-        //categoryHolder.tx_id.setText(categories.getId());
         playerHolder.playerNumber.setText( "#"+Integer.toString(player.getNumber()));
         playerHolder.playerPTS.setText(Integer.toString(0));
-        //categoryHolder.tx_description.setText(categories.getDescription());
-
         return row;
     }
 
     static class PlayerHolder{
         TextView playerNumber, playerPTS;
         Button plusOne, plusTwo, plusThree;
+        TextView teamA, teamB;
     }
 }
 
