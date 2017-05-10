5b7272898ba62ce0b85a8a01917f050b2a7b1dc0
Edvinas <test@test.com>
2017-05-10 16:52:35 +0300

    Refactored code, tweaked UI, cleaned project. Ready to submit

--- app/src/main/java/com/example/android/project2/MainActivity.java
+++ app/src/main/java/com/example/android/project2/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     ListView listViewA;
 
     ListView listViewB;
-    PlayerAdapter playerAdapterB;
 
     EditText editNumberA;
     EditText editNumberB;
@@ -38,22 +37,41 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     Button resetScoresA;
     Button resetScoresB;
 
-    Button addPlayerA;
-    Button addPlayerB;
+    Button addPlayerAButton;
+    Button addPlayerBButton;
 
     PlayerAdapter playerAdapterA;
+    PlayerAdapter playerAdapterB;
+
+    SharedPreferences sharedPrefScoreA;
+    SharedPreferences sharedPrefScoreB;
+    SharedPreferences sharedPrefTeamName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addPlayerA = (Button) findViewById(R.id.resetScoresA);
-        addPlayerB = (Button) findViewById(R.id.resetScoresB);
+        findViews();
 
-        resetScoresA = (Button) findViewById(R.id.addPlayerA);
-        resetScoresB = (Button) findViewById(R.id.addPlayerB);
+        setPlayerAdapters();
 
+        setSharedPreferences();
+
+        registerSharedPrefChangeListener();
+
+        clearSharedPreferences();
+
+        addPlayerAOnClick();
+        addPlayerBOnClick();
+    }
+
+    private void findViews() {
+        addPlayerAButton = (Button) findViewById(R.id.addPlayerA);
+        addPlayerBButton = (Button) findViewById(R.id.addPlayerB);
+
+        resetScoresA = (Button) findViewById(R.id.resetScoresA);
+        resetScoresB = (Button) findViewById(R.id.resetScoresB);
 
         teamScoreA = (TextView) findViewById(R.id.scoreA);
         teamScoreB = (TextView) findViewById(R.id.scoreB);
@@ -72,21 +90,27 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
         listViewA = (ListView) findViewById(R.id.teamAList);
         listViewB = (ListView) findViewById(R.id.teamBList);
+    }
 
+    private void setPlayerAdapters() {
         playerAdapterA = new PlayerAdapter(this, R.layout.row_player);
         listViewA.setAdapter(playerAdapterA);
 
         playerAdapterB = new PlayerAdapter(this, R.layout.row_player);
         listViewB.setAdapter(playerAdapterB);
+    }
 
-        SharedPreferences sharedPrefScoreA = getSharedPreferences("scoreA", Context.MODE_PRIVATE);
-        SharedPreferences sharedPrefScoreB = getSharedPreferences("scoreB", Context.MODE_PRIVATE);
+    private void setSharedPreferences() {
+        sharedPrefScoreA = getSharedPreferences("scoreA", Context.MODE_PRIVATE);
+        sharedPrefScoreB = getSharedPreferences("scoreB", Context.MODE_PRIVATE);
 
+    }
+
+    private void registerSharedPrefChangeListener() {
         sharedPrefScoreA.registerOnSharedPreferenceChangeListener(this);
         sharedPrefScoreB.registerOnSharedPreferenceChangeListener(this);
-
-        clearSharedPreferences();
     }
+
     public void clearSharedPreferences() {
         SharedPreferences sharedPrefScoreA = getSharedPreferences("scoreA", Context.MODE_PRIVATE);
         SharedPreferences sharedPrefScoreB = getSharedPreferences("scoreB", Context.MODE_PRIVATE);
@@ -106,58 +130,123 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         SharedPreferences sharedPrefPlayers = getSharedPreferences("players", Context.MODE_PRIVATE);
         sharedPrefPlayers.edit().clear().apply();
     }
-    public void addPlayerA(View view) {
-        if (editNumberA.getText().length() != 0 && Integer.parseInt(editNumberA.getText().toString()) > 0 && Integer.parseInt(editNumberA.getText().toString()) < 100 && editTeamA.getText().length() != 0) {
-            SharedPreferences sharedPrefNameA = getSharedPreferences("nameA", Context.MODE_PRIVATE);
-            SharedPreferences.Editor edit = sharedPrefNameA.edit();
-            edit.putString("nameA", editTeamA.getText().toString());
-            edit.apply();
-            boolean duplicate = false;
-            Player player = new Player(Integer.parseInt(editNumberA.getText().toString()), editTeamA.getText().toString(), 0);
-            if (playerAdapterA.isEmpty()) {
+
+    private void addPlayerAOnClick() {
+        addPlayerAButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String playerTeam = "A";
+                if (validateAddPlayerInfo(playerTeam)) {
+                    Player player = getAndCreatePlayer(playerTeam);
+                    if (playerAdapterA.isEmpty()) {
+                        addPlayerEmptyAdapter(player, playerTeam);
+                    } else if (!playerAdapterA.isEmpty())
+                        addPlayerANotEmptyAdapter(player, playerTeam);
+                } else
+                    Toast.makeText(MainActivity.this, "Please input player number 0-99 and a team name", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void addPlayerBOnClick() {
+        addPlayerBButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String playerTeam = "B";
+                if (validateAddPlayerInfo(playerTeam)) {
+                    Player player = getAndCreatePlayer(playerTeam);
+                    if (playerAdapterB.isEmpty()) {
+                        addPlayerEmptyAdapter(player, playerTeam);
+                    } else if (!playerAdapterB.isEmpty())
+                        addPlayerANotEmptyAdapter(player, playerTeam);
+                } else
+                    Toast.makeText(MainActivity.this, "Please input player number 0-99 and a team name", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean validateAddPlayerInfo(String playerTeam) {
+        if (playerTeam.equals("A")) {
+            if (editNumberA.getText().length() != 0 &&
+                    Integer.parseInt(editNumberA.getText().toString()) > 0 &&
+                    Integer.parseInt(editNumberA.getText().toString()) < 100 &&
+                    editTeamA.getText().length() != 0)
+                return true;
+            else return false;
+        } else {
+            if (editNumberB.getText().length() != 0 &&
+                    Integer.parseInt(editNumberB.getText().toString()) > 0 &&
+                    Integer.parseInt(editNumberB.getText().toString()) < 100 &&
+                    editTeamB.getText().length() != 0)
+                return true;
+            else return false;
+        }
+    }
+
+    public Player getAndCreatePlayer(String playerTeam) {
+        if (playerTeam.equals("A")) {
+            setSharedPrefTeamName("nameA", editTeamA.getText().toString());
+            Player player = new Player(Integer.parseInt(editNumberA.getText().toString()),
+                    editTeamA.getText().toString(), 0);
+            return player;
+        } else {
+            Player player = new Player(Integer.parseInt(editNumberB.getText().toString()),
+                    editTeamB.getText().toString(), 0);
+            return player;
+
+        }
+    }
+
+    private void addPlayerEmptyAdapter(Player player, String playerTeam) {
+        if (playerTeam.equals("A")) {
+            setPtsNrHeadersIfEmpty(playerTeam);
+            playerAdapterA.add(player);
+        } else {
+            setPtsNrHeadersIfEmpty(playerTeam);
+            playerAdapterB.add(player);
+        }
+    }
+
+    private void setPtsNrHeadersIfEmpty(String playerTeam) {
+        if (playerTeam.equals("A")) {
+            if (nrHeaderA.getText().toString().isEmpty() || ptsHeaderA.getText().toString().isEmpty()) {
                 nrHeaderA.setText(R.string.NR);
                 ptsHeaderA.setText(R.string.PTS);
-                playerAdapterA.add(player);
-            } else if (!playerAdapterA.isEmpty()) {
-                for (int i = 0; i < playerAdapterA.getCount(); i++) {
-                    if (playerAdapterA.getItem(i).equals(player)) {
-                        Toast.makeText(this, "Duplicate player number, players must have unique number", Toast.LENGTH_SHORT).show();
-                        duplicate = true;
-                    }
-                }
-                if (!duplicate) {
-                    playerAdapterA.add(player);
-                }
             }
-        } else
-            Toast.makeText(MainActivity.this, "Please input player number 0-99 and a team name", Toast.LENGTH_SHORT).show();
-    }
-    public void addPlayerB(View view) {
-        if (editNumberB.getText().length() != 0 && Integer.parseInt(editNumberB.getText().toString()) > 0 && Integer.parseInt(editNumberB.getText().toString()) < 100 && editTeamB.getText().length() != 0) {
-            SharedPreferences sharedPrefNameB = getSharedPreferences("nameB", Context.MODE_PRIVATE);
-            SharedPreferences.Editor edit = sharedPrefNameB.edit();
-            edit.putString("nameB", editTeamB.getText().toString());
-            edit.apply();
-            boolean duplicate = false;
-            Player player = new Player(Integer.parseInt(editNumberB.getText().toString()), editNumberB.getText().toString(), 0);
-            if (playerAdapterB.isEmpty()) {
+        } else {
+            if (nrHeaderB.getText().toString().isEmpty() || ptsHeaderB.getText().toString().isEmpty()) {
                 nrHeaderB.setText(R.string.NR);
                 ptsHeaderB.setText(R.string.PTS);
-                playerAdapterB.add(player);
-            } else if (!playerAdapterB.isEmpty()) {
-                for (int i = 0; i < playerAdapterB.getCount(); i++) {
-                    if (playerAdapterB.getItem(i).equals(player)) {
-                        Toast.makeText(this, "Duplicate player number, players must have unique number", Toast.LENGTH_SHORT).show();
-                        duplicate = true;
-                    }
-                }
-                if (!duplicate) {
-                    playerAdapterB.add(player);
+            }
+        }
+    }
+
+    private void addPlayerANotEmptyAdapter(Player player, String playerTeam) {
+        if (playerTeam.equals("A")) {
+            for (int i = 0; i < playerAdapterA.getCount(); i++) {
+                if (playerAdapterA.getItem(i).equals(player)) {
+                    Toast.makeText(MainActivity.this, "Duplicate player number, players must have unique number", Toast.LENGTH_SHORT).show();
+                    return;
                 }
             }
-        } else
-            Toast.makeText(MainActivity.this, "Please input player number 0-99 and a team name", Toast.LENGTH_SHORT).show();
+            playerAdapterA.add(player);
+        } else {
+            for (int i = 0; i < playerAdapterB.getCount(); i++) {
+                if (playerAdapterB.getItem(i).equals(player)) {
+                    Toast.makeText(MainActivity.this, "Duplicate player number, players must have unique number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+            playerAdapterB.add(player);
+        }
     }
+
+    private void setSharedPrefTeamName(String name, String value) {
+        sharedPrefTeamName = getSharedPreferences(name, Context.MODE_PRIVATE);
+            SharedPreferences.Editor edit = sharedPrefTeamName.edit();
+            edit.putString(name, value);
+            edit.apply();
+    }
+
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals("scoreA")) {
@@ -166,21 +255,31 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
             teamScoreB.setText(Integer.toString(sharedPreferences.getInt("scoreB", 0)));
         }
     }
+
     public void resetB(View view) {
         SharedPreferences scoreB = getSharedPreferences("scoreB", Context.MODE_PRIVATE);
         SharedPreferences.Editor edit = scoreB.edit();
         edit.putInt("scoreB", 0);
         edit.apply();
+        resetEverythingBSide(scoreB);
+    }
+
+    private void resetEverythingBSide(SharedPreferences scoreB) {
         teamScoreB.setText(Integer.toString(scoreB.getInt("scoreB", 0)));
         nrHeaderB.setText("");
         ptsHeaderB.setText("");
         playerAdapterB.remove();
     }
+
     public void resetA(View view) {
         SharedPreferences scoreA = getSharedPreferences("scoreA", Context.MODE_PRIVATE);
         SharedPreferences.Editor edit = scoreA.edit();
         edit.putInt("scoreA", 0);
         edit.apply();
+        resetEverythingASide(scoreA);
+    }
+
+    private void resetEverythingASide(SharedPreferences scoreA) {
         teamScoreA.setText(Integer.toString(scoreA.getInt("scoreA", 0)));
         nrHeaderA.setText("");
         ptsHeaderA.setText("");
--- app/src/main/java/com/example/android/project2/PlayerAdapter.java
+++ app/src/main/java/com/example/android/project2/PlayerAdapter.java
@@ -2,8 +2,6 @@ package com.example.android.project2;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,23 +20,20 @@ import static com.example.android.project2.R.id.playerPTS;
  * Custom ArrayAdapter class to adjust ListView row (row_player) which shows Player stats.
  * credits : PRABEESH R K https://www.youtube.com/watch?v=cyk_ht8z6IA&t=176s
  */
-
 public class PlayerAdapter extends ArrayAdapter {
     private List list = new ArrayList();
-    List<Integer> pts = new ArrayList<>();
+    private List<Integer> pts = new ArrayList<>();
 
-    public PlayerAdapter(Context context, int resource) {
+    PlayerAdapter(Context context, int resource) {
         super(context, resource);
     }
 
-    SharedPreferences sharedPrefA = getContext().getSharedPreferences("scoreA", Context.MODE_PRIVATE);
-    SharedPreferences sharedPrefB = getContext().getSharedPreferences("scoreB", Context.MODE_PRIVATE);
-    SharedPreferences sharedPrefName = getContext().getSharedPreferences("nameA", Context.MODE_PRIVATE);
-    boolean teamA;
-    //int undoClicks;
+    private SharedPreferences sharedPrefA = getContext().getSharedPreferences("scoreA", Context.MODE_PRIVATE);
+    private SharedPreferences sharedPrefB = getContext().getSharedPreferences("scoreB", Context.MODE_PRIVATE);
+    private SharedPreferences sharedPrefName = getContext().getSharedPreferences("nameA", Context.MODE_PRIVATE);
+    private boolean isTeamA;
 
-
-    public void add(Player object) {
+    void add(Player object) {
         super.add(object);
         list.add(object);
     }
@@ -70,49 +65,21 @@ public class PlayerAdapter extends ArrayAdapter {
             row = layoutInflater.inflate(R.layout.row_player, parent, false);
             playerHolder = new PlayerHolder();
 
-            playerHolder.playerNumber = (TextView) row.findViewById(R.id.playerNumber);
-            playerHolder.playerPTS = (TextView) row.findViewById(playerPTS);
+            findViewsInLayout(row, playerHolder);
 
-            playerHolder.plusOne = (Button) row.findViewById(R.id.plusOne);
-            playerHolder.plusTwo = (Button) row.findViewById(R.id.plusTwo);
-            playerHolder.plusThree = (Button) row.findViewById(R.id.plusThree);
-            playerHolder.reset = (Button) row.findViewById(R.id.reset);
-            playerHolder.undo = (Button) row.findViewById(R.id.undo);
-            playerHolder.delete = (Button) row.findViewById(R.id.delete);
             Player player = (Player) this.getItem(position);
+
             pts.add(player.getScore());
-            //Toast.makeText(getContext(), player.getTeam(), Toast.LENGTH_SHORT).show();
-            //Toast.makeText(getContext(), sharedPrefName.getString("nameB",""), Toast.LENGTH_SHORT).show();
 
-            if (sharedPrefName.getString("nameA", "").equals(player.getTeam())) {
-                teamA = true;
-                playerHolder.delete.getBackground().setColorFilter(Color.parseColor("#dbf2d7"), PorterDuff.Mode.DARKEN);
-                playerHolder.undo.getBackground().setColorFilter(Color.parseColor("#dbf2d7"), PorterDuff.Mode.DARKEN);
-                playerHolder.reset.getBackground().setColorFilter(Color.parseColor("#dbf2d7"), PorterDuff.Mode.DARKEN);
+            isTeamA = (sharedPrefName.getString("nameA", "").equals(player.getTeam())) ? true : false ;
 
-                playerHolder.plusOne.getBackground().setColorFilter(Color.parseColor("#dbf2d7"), PorterDuff.Mode.DARKEN);
-                playerHolder.plusTwo.getBackground().setColorFilter(Color.parseColor("#dbf2d7"), PorterDuff.Mode.DARKEN);
-                playerHolder.plusThree.getBackground().setColorFilter(Color.parseColor("#dbf2d7"), PorterDuff.Mode.DARKEN);
-
-            } else {
-                teamA = false;
-                playerHolder.delete.getBackground().setColorFilter(Color.parseColor("#d6e1f1"), PorterDuff.Mode.DARKEN);
-                playerHolder.undo.getBackground().setColorFilter(Color.parseColor("#d6e1f1"), PorterDuff.Mode.DARKEN);
-                playerHolder.reset.getBackground().setColorFilter(Color.parseColor("#d6e1f1"), PorterDuff.Mode.DARKEN);
-
-                playerHolder.plusOne.getBackground().setColorFilter(Color.parseColor("#d6e1f1"), PorterDuff.Mode.DARKEN);
-                playerHolder.plusTwo.getBackground().setColorFilter(Color.parseColor("#d6e1f1"), PorterDuff.Mode.DARKEN);
-                playerHolder.plusThree.getBackground().setColorFilter(Color.parseColor("#d6e1f1"), PorterDuff.Mode.DARKEN);
-            }
-            //undoClicks = 0;
             playerHolder.plusOne.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //pts.add(Integer.parseInt(playerHolder.playerPTS.getText().toString()));
-                    int ptss = Integer.parseInt(playerHolder.playerPTS.getText().toString());
-                    pts.add(++ptss);
-                    //Toast.makeText(getContext(),Integer.toString(pts.get(pts.size()-1)),Toast.LENGTH_SHORT).show();
-                    if (teamA) {
+                    int tempPts = Integer.parseInt(playerHolder.playerPTS.getText().toString());
+                    tempPts += 1;
+                    pts.add(tempPts);
+                    if (isTeamA) {
                         SharedPreferences.Editor edit = sharedPrefA.edit();
                         edit.putInt("scoreA", sharedPrefA.getInt("scoreA", 0) + 1);
                         edit.apply();
@@ -121,20 +88,16 @@ public class PlayerAdapter extends ArrayAdapter {
                         edit.putInt("scoreB", sharedPrefB.getInt("scoreB", 0) + 1);
                         edit.apply();
                     }
-                    /*for (int i = 0; i < pts.size(); i++){
-                        Toast.makeText(getContext(),Integer.toString(1+i)+" "+Integer.toString(pts.get(i)),Toast.LENGTH_SHORT).show();
-                    }*/
                     playerHolder.playerPTS.setText(Integer.toString(pts.get(pts.size() - 1)));
                 }
             });
             playerHolder.plusTwo.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //pts.add(Integer.parseInt(playerHolder.playerPTS.getText().toString()));
-                    int ptss = Integer.parseInt(playerHolder.playerPTS.getText().toString());
-                    ptss += 2;
-                    pts.add(ptss);
-                    if (teamA) {
+                    int tempPts = Integer.parseInt(playerHolder.playerPTS.getText().toString());
+                    tempPts += 2;
+                    pts.add(tempPts);
+                    if (isTeamA) {
                         SharedPreferences.Editor edit = sharedPrefA.edit();
                         edit.putInt("scoreA", sharedPrefA.getInt("scoreA", 0) + 2);
                         edit.apply();
@@ -143,20 +106,16 @@ public class PlayerAdapter extends ArrayAdapter {
                         edit.putInt("scoreB", sharedPrefB.getInt("scoreB", 0) + 2);
                         edit.apply();
                     }
-                    /*for (int i = 0; i < pts.size(); i++){
-                        Toast.makeText(getContext(),Integer.toString(1+i)+" "+Integer.toString(pts.get(i)),Toast.LENGTH_SHORT).show();
-                    }*/
                     playerHolder.playerPTS.setText(Integer.toString(pts.get(pts.size() - 1)));
                 }
             });
             playerHolder.plusThree.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //pts.add(Integer.parseInt(playerHolder.playerPTS.getText().toString()));
-                    int ptss = Integer.parseInt(playerHolder.playerPTS.getText().toString());
-                    ptss += 3;
-                    pts.add(ptss);
-                    if (teamA) {
+                    int tempPts = Integer.parseInt(playerHolder.playerPTS.getText().toString());
+                    tempPts += 3;
+                    pts.add(tempPts);
+                    if (isTeamA) {
                         SharedPreferences.Editor edit = sharedPrefA.edit();
                         edit.putInt("scoreA", sharedPrefA.getInt("scoreA", 0) + 3);
                         edit.apply();
@@ -165,16 +124,13 @@ public class PlayerAdapter extends ArrayAdapter {
                         edit.putInt("scoreB", sharedPrefB.getInt("scoreB", 0) + 3);
                         edit.apply();
                     }
-                    /*for (int i = 0; i < pts.size(); i++){
-                        Toast.makeText(getContext(),Integer.toString(1+i)+" "+Integer.toString(pts.get(i)),Toast.LENGTH_SHORT).show();
-                    }*/
                     playerHolder.playerPTS.setText(Integer.toString(pts.get(pts.size() - 1)));
                 }
             });
             playerHolder.reset.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (teamA) {
+                    if (isTeamA) {
                         SharedPreferences.Editor edit = sharedPrefA.edit();
                         edit.putInt("scoreA", sharedPrefA.getInt("scoreA", 0) - Integer.parseInt(playerHolder.playerPTS.getText().toString()));
                         edit.apply();
@@ -191,51 +147,12 @@ public class PlayerAdapter extends ArrayAdapter {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(getContext(), "Feature coming soon", Toast.LENGTH_SHORT).show();
-                   /* //Toast.makeText(getContext(),Integer.toString(pts.size()),Toast.LENGTH_SHORT).show();
-
-                    if (pts.size() - undoClicks <= 0){
-                        Toast.makeText(getContext(),"There is nothing to undo",Toast.LENGTH_SHORT).show();
-                    }else if (pts.size() - undoClicks > 0){
-                        undoClicks +=1;
-                        if (teamA){
-                            SharedPreferences.Editor edit = sharedPrefA.edit();
-                            edit.putInt("scoreA", pts.get(pts.size()-undoClicks-1));
-                            edit.apply();
-                        }
-                        else {
-                            SharedPreferences.Editor edit = sharedPrefB.edit();
-                            edit.putInt("scoreB", pts.get(pts.size()-undoClicks-1));
-                            edit.apply();
-                        }
-                        playerHolder.playerPTS.setText(Integer.toString(pts.get(pts.size()-undoClicks-1)));
-                        //undoClicks = 0;
-                    }*/
                 }
             });
             playerHolder.delete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(getContext(), "Feature coming soon", Toast.LENGTH_SHORT).show();
-                    /*list.remove(position);
-                    pts.remove(position);
-                    if (teamA){
-                        SharedPreferences.Editor edit = sharedPrefA.edit();
-                        edit.putInt("scoreA", sharedPrefA.getInt("scoreA",0)-Integer.parseInt(playerHolder.playerPTS.getText().toString()));
-                        edit.apply();
-                    }
-                    else {
-                        SharedPreferences.Editor edit = sharedPrefB.edit();
-                        edit.putInt("scoreB", sharedPrefB.getInt("scoreB",0)-Integer.parseInt(playerHolder.playerPTS.getText().toString()));
-                        edit.apply();
-                    }
-                    if (list.size() == 0)
-                        playerHolder.playerPTS.setText("0");
-                    else if (list.size() > 0){
-                        playerHolder.playerPTS.setText(Integer.toString(pts.get(position+1)));
-                    }
-                    //Toast.makeText(getContext(),Integer.toString(list.size()),Toast.LENGTH_SHORT).show();
-                    //playerHolder.playerPTS.setText("0");
-                    notifyDataSetChanged();*/
                 }
             });
             row.setTag(playerHolder);
@@ -244,8 +161,18 @@ public class PlayerAdapter extends ArrayAdapter {
         }
         Player player = (Player) this.getItem(position);
         playerHolder.playerNumber.setText("#" + Integer.toString(player.getNumber()));
-        //pts = 0;
         return row;
+    }
+
+    private void findViewsInLayout(View row, PlayerHolder playerHolder) {
+        playerHolder.playerNumber = (TextView) row.findViewById(R.id.playerNumber);
+        playerHolder.playerPTS = (TextView) row.findViewById(playerPTS);
+        playerHolder.plusOne = (Button) row.findViewById(R.id.plusOne);
+        playerHolder.plusTwo = (Button) row.findViewById(R.id.plusTwo);
+        playerHolder.plusThree = (Button) row.findViewById(R.id.plusThree);
+        playerHolder.reset = (Button) row.findViewById(R.id.reset);
+        playerHolder.undo = (Button) row.findViewById(R.id.undo);
+        playerHolder.delete = (Button) row.findViewById(R.id.delete);
     }
 
     public static class PlayerHolder {
